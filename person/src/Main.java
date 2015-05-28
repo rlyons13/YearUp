62a885d3c7e9536f79a02da76305a995330368a8
rlyons13 <rlyons@atl.yearup.org>
2015-05-28 00:39:34 -0400

    message

---
+++ person/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Person;
+		GrowsOlder firstGrowth = new Age();
+		System.out.println(firstGrowth.GetAge() + "to" + firstGrowth.GetName());
+
+			
+	}
+
+}
---
+++ person/src/Person.java
@@ -0,0 +1,65 @@
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+		private String m_name;
+		private int m_age;
+		private String m_phonenumber;
+		private String m_address;
+
+		
+	public Person(String name, String phonenumber, String address, int age)
+	{
+			 SetName(name);
+			 SetAge(age);
+			 SetPhonenumber(phonenumber);
+			 Setaddress(address);
+		}
+			public String GetName()
+			{
+				return m_name;
+			}
+			private void SetName(String name)
+			{
+				m_name = name;
+			}
+			public int GetAge()
+			{
+				return m_age;
+			}
+			private void SetAge(int age)
+			{
+				m_age = age;
+			}
+			public String GetPhonenumber()
+			{
+				return m_phonenumber;	
+			}
+			private void SetPhonenumber(String phonenumber)
+			{
+				m_phonenumber = phonenumber;
+			}
+			public String GetAddress()
+			{
+				return m_address;
+			}
+			private void Setaddress(String address)
+			{
+				m_address = address;
+			}
+			public double GrowsOlder(int age)
+			{
+				double Age = 1 + age;
+				
+				return Age;
+			}
+	
+
+		
+
+	 
+	}
+
+
