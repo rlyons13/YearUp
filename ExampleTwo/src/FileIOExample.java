9e8dc6c8dfef34f8364040683c258758259030cc
rlyons13 <rlyons@atl.yearup.org>
2015-06-26 21:50:50 -0400

    message

---
+++ ExampleTwo/src/FileIOExample.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import 
+
+public class FileIOExample {
+
+	public static void main(String[] args) 
+	{
+		Read();
+	}
+	public static void  Read()
+	{
+		FileReader filereader = null;
+		
+		try
+		{
+			fileReader = new FileReader ("C:\\users\\rlyons-price13\\readme.txt");
+			BufferedReader bufferedReader = new BufferedReader (fileReader);
+			String iReadThis = BufferedReader
+		}
+	}
+}
